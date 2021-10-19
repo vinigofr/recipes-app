657e4a6f1f35594b8b2ea881af88e2ae6bb02b9c
Vinicius Gouveia <freitas.viniciuspk@outlook.com.br>
2021-10-19 20:38:50 -0300

    Updates

---
+++ precache-manifest.0a6f7ecb32e0f550f5cb112c00b5efcc.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "93ceecf577868ea432d46713a44ddfcd",
+    "url": "/recipes-app/index.html"
+  },
+  {
+    "revision": "ce939f8b78007aa79475",
+    "url": "/recipes-app/static/css/2.95258f03.chunk.css"
+  },
+  {
+    "revision": "affe4e4232c931288c6a",
+    "url": "/recipes-app/static/css/main.f919a1ed.chunk.css"
+  },
+  {
+    "revision": "ce939f8b78007aa79475",
+    "url": "/recipes-app/static/js/2.74bc69ce.chunk.js"
+  },
+  {
+    "revision": "737926aa5f30acaedb866e79ac0007c9",
+    "url": "/recipes-app/static/js/2.74bc69ce.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "affe4e4232c931288c6a",
+    "url": "/recipes-app/static/js/main.31877d5f.chunk.js"
+  },
+  {
+    "revision": "25993f9b630f63b44851",
+    "url": "/recipes-app/static/js/runtime-main.1228032d.js"
+  },
+  {
+    "revision": "b89133460c9aef111d6e773396b5917d",
+    "url": "/recipes-app/static/media/blackHeartIcon.b8913346.svg"
+  },
+  {
+    "revision": "b3193eb67b07c1465c25636daf0c184f",
+    "url": "/recipes-app/static/media/exploreIcon.b3193eb6.png"
+  },
+  {
+    "revision": "bc58ebdc8046617aef4d261a9aa01355",
+    "url": "/recipes-app/static/media/pngwing.com.bc58ebdc.png"
+  },
+  {
+    "revision": "74e487e504d5cf16d7832bef2fbacfea",
+    "url": "/recipes-app/static/media/profileIcon.74e487e5.svg"
+  },
+  {
+    "revision": "db81213596cb3beb56440bb39aa70c8c",
+    "url": "/recipes-app/static/media/searchIcon.db812135.svg"
+  },
+  {
+    "revision": "06215ed4cae23455c02abc804445ba74",
+    "url": "/recipes-app/static/media/shareIcon.06215ed4.svg"
+  },
+  {
+    "revision": "ea3b6ba893ba3908dd84f6170b75545e",
+    "url": "/recipes-app/static/media/whiteHeartIcon.ea3b6ba8.svg"
+  }
+]);
